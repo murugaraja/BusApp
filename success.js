53731c44e7512fef9581f1a056a4c3c5e166afe6
murugaraja <murugarajamca@gmail.com>
2016-02-08 14:51:48 +0530

    Result only not comming

--- success.js
+++ success.js
@@ -1,139 +1,146 @@
-    var fulldetails = '{"w2" : {"cmpudur" : "02:00 am","kp" : "03:00 am","desure" : "04:00 am"},"narayana" : {"cmpudur" : "01:00 am","kp" : "02:00 am","desure" : "03:00 am"},"w2r" : {"desure" : "04:00 pm","kp" : "06:00 pm","cmpudur" : "08:00 pm"},"narayanar" : {"desure" : "01:00 pm","kp" : "02:00 pm","cmpudur" : "03:00 pm"}}'; 
-    
-    var fulldetailsObj = JSON.parse(fulldetails);
-    var allplaces = [];
+var allplaces = [];
+var fulldetails;
+var fulldetailsObj;
 
-    for ( var bus in fulldetailsObj) {
-        busName = bus;
-        routeDetails = fulldetailsObj[bus];
-        for ( var route in routeDetails) {
-            a = route;
-            b = routeDetails[route].toString();
-            allplaces.push(a);
-        }
-    }
+$(document).ready(function(){
+	fulldetailsObj = JSON.parse(sampledata);
+	
+	for ( var bus in fulldetailsObj) {
+		busName = bus;
+		routeDetails = fulldetailsObj[bus];
+		for ( var route in routeDetails) {
+			a = route;
+			b = routeDetails[route].toString();
+			allplaces.push(a);
+		}
+	}
+}) 
 
-    function eliminateDuplicates(arr) {
-        var i, len = arr.length, out = [], obj = {};
-        for (i = 0; i < len; i++) {
-            obj[arr[i]] = 0;
-        }
-        for (i in obj) {
-            out.push(i);
-        }
-        return out;
-    }
+function eliminateDuplicates(arr) {
+	var i, len = arr.length, out = [], obj = {};
+	for (i = 0; i < len; i++) {
+		obj[arr[i]] = 0;
+	}
+	for (i in obj) {
+		out.push(i);
+	}
+	return out;
+}
 
-    function myFunction() {
-        var allplaces1 = eliminateDuplicates(allplaces);
-        var sel = document.getElementById('source');
-        for (var i = 0; i < allplaces1.length; i++) {
-            var opt = document.createElement('option');
-            opt.innerHTML = allplaces1[i];
-            opt.value = allplaces1[i];
-            sel.appendChild(opt);
-        }
-document.getElementById("source1").innerHTML = document.getElementById("source").innerHTML;
-        fetchTime();
-    }
+function myFunction() {
+	var allplaces1 = eliminateDuplicates(allplaces);
+	allplaces1.sort();
+	var sel = document.getElementById('source');
+	for (var i = 0; i < allplaces1.length; i++) {
+		var opt = document.createElement('option');
+		opt.innerHTML = allplaces1[i];
+		opt.value = allplaces1[i];
+		sel.appendChild(opt);
+	}
+	document.getElementById("source1").innerHTML = document
+			.getElementById("source").innerHTML;
+	fetchTime();
+}
 
-    function fetchTime() {
-        var d = new Date();
-        var h = d.getHours();
-        var m = d.getMinutes();
-        document.getElementById("time").innerHTML = h + ":" + m;
-    }
-    var availableList = [];
+function fetchTime() {
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	document.getElementById("time").innerHTML = h + ":" + m;
+}
 
-    function getDetails() {
+var availableList = [];
 
-        availableList = [];
+function getDetails() {
 
-        var from = document.getElementById("source").value;
-        var to = document.getElementById("source1").value;
+	availableList = [];
 
-        var i = 0;
+	var from = document.getElementById("source").value;
+	var to = document.getElementById("source1").value;
 
-        for ( var bus in fulldetailsObj) {
-            busName = bus;
-            routeDetails = fulldetailsObj[bus];
-            var bfrom;
+	var i = 0;
 
-            var sourceTime;
+	for ( var bus in fulldetailsObj) {
+		busName = bus;
+		routeDetails = fulldetailsObj[bus];
+		var bfrom;
 
-            for ( var route in routeDetails) {
+		var sourceTime;
 
-                a = route;
-                b = routeDetails[route].toString();
+		for ( var route in routeDetails) {
 
-                if (from == a) {
-                    bfrom = true;
-                    sourceTime = b;
-                    continue;
-                }
-                if (bfrom == true) {
-                    if (to == a) {
-                        availableList[i] = new Array(2);
-                        availableList[i][0] = bus;
-                        availableList[i][1] = sourceTime;
-                        i++;
-                    }
-                    bfrom = false;
-                }
-            }
-        }
-        sortTimings();
-        makelist();
-    }
+			a = route;
+			b = routeDetails[route].toString();
 
-    function sorting(a, b) {
-        return new Date('1970/01/01 ' + a) - new Date('1970/01/01 ' + b);
-    }
+			if (from == a) {
+				bfrom = true;
+				sourceTime = b;
+				continue;
+			}
+			if (bfrom == true) {
+				if (to == a) {
+					availableList[i] = new Array(2);
+					availableList[i][0] = bus;
+					availableList[i][1] = sourceTime;
+					i++;
+				}
+				bfrom = false;
+			}
+		}
+	}
+	sortTimings();
+	makelist();
+}
 
-    function sortTimings() {
+function sorting(a, b) {
+	return new Date('1970/01/01 ' + a) - new Date('1970/01/01 ' + b);
+}
 
-        for (i = 0; i < availableList.length - 1; i++) {
-            for (j = 0; j < availableList.length - 1; j++) {
-                if (availableList.length != 0 && availableList.length != 1) {
+function sortTimings() {
 
-                    if (sorting(availableList[j][1], availableList[j + 1][1]) > 0) {
+	for (i = 0; i < availableList.length - 1; i++) {
+		for (j = 0; j < availableList.length - 1; j++) {
+			if (availableList.length != 0 && availableList.length != 1) {
 
-                        var temp = availableList[j][1];
-                        var temp1 = availableList[j][0];
+				if (sorting(availableList[j][1], availableList[j + 1][1]) > 0) {
 
-                        availableList[j][1] = availableList[j + 1][1];
-                        availableList[j][0] = availableList[j + 1][0];
+					var temp = availableList[j][1];
+					var temp1 = availableList[j][0];
 
-                        availableList[j + 1][1] = temp;
-                        availableList[j + 1][0] = temp1;
+					availableList[j][1] = availableList[j + 1][1];
+					availableList[j][0] = availableList[j + 1][0];
 
-                    }
-                }
-            }
-        }
-    }
+					availableList[j + 1][1] = temp;
+					availableList[j + 1][0] = temp1;
 
-    function makelist() {
+				}
+			}
+		}
+	}
+}
 
-        var listElement = document.getElementById("list");
+function makelist() {
 
-        listElement.innerHTML = '';
+	var listElement = document.getElementById("list");
 
-        var numberOfListItems = availableList.length;
+	listElement.innerHTML = '';
 
-	if(numberOfListItems==0){
+	var numberOfListItems = availableList.length;
+
+	if (numberOfListItems == 0) {
 		alert("No result found!");
 	}
 
-        for (var i = 0; i < numberOfListItems; ++i) {
-            var listItem = document.createElement("li");
+	for (var i = 0; i < numberOfListItems; ++i) {
+		var listItem = document.createElement("li");
 
-	    if(i%2==0)
-            listItem.className = "list-group-item list-group-item-success";
-            else
-            listItem.className = "list-group-item list-group-item-danger";
+		if (i % 2 == 0)
+			listItem.className = "list-group-item list-group-item-success";
+		else
+			listItem.className = "list-group-item list-group-item-danger";
 
-            listItem.innerHTML = availableList[i][1] + " -:- " + availableList[i][0];
-            listElement.appendChild(listItem);
-        }
-    }
+		listItem.innerHTML = availableList[i][1] + " -:- "
+				+ availableList[i][0];
+		listElement.appendChild(listItem);
+	}
+}
